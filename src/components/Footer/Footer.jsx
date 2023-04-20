7220b20b323eb0f645970f2bf3c7e43f6779c40a
vbhatkar2506 <vbhatkar2506@gmail.com>
2023-04-20 13:05:09 +0530

    header/footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+import styles from './Footer.module.css'
+function Footer () {
+return (
+        <div className={styles.footer}>
+            <h2>Footer</h2>
+        </div>
+);
+}
+export default Footer;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import styles from './Header.module.css'
+
+function Header() {
+return (
+<div className={styles.Header}>
+    <h1>Hello</h1>
+</div>
+);
+}
+export default Header;
